e487fdb0676acfddf7780a3049535d7b294bbc8d
Oliver Scott <olivercscott@gmail.com>
2019-08-15 19:40:55 -0700

    fw/b: Add setting to allow tethering to use VPN upstreams
    Change-Id: I77ed0142e653f4993486eea44e4dac21e3f67f17

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -308,6 +308,7 @@ public class SecureSettings {
         Settings.Secure.ON_DEVICE_INTELLIGENCE_UNBIND_TIMEOUT_MS,
         Settings.Secure.ON_DEVICE_INFERENCE_UNBIND_TIMEOUT_MS,
         Settings.Secure.ON_DEVICE_INTELLIGENCE_IDLE_TIMEOUT_MS,
+        Settings.Secure.TETHERING_ALLOW_VPN_UPSTREAMS,
         Settings.Secure.FIRST_ON_DEVICE_MODELS_DOWNLOADED_TIME,
         Settings.Secure.CONTENT_SAFETY_IDLE_TIMEOUT_MS,
         Settings.Secure.CONTENT_SAFETY_UNBIND_TIMEOUT_MS,
