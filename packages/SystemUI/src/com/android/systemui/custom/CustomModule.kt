b602ccd06062c2f0531dc7b4b023ae2730e4bc1a
Dil3mm4 <dil3mm4.dev@gmail.com>
2022-05-05 00:31:24 +0530

    SystemUI: Add Autobrightness tile
    [johnmart19]
     - Rework for A12.1 environment
     - Launch Display Settings on long click
    
    [hridaya]
     - Fix building on A14 QPR3
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>
    Change-Id: Id395f0da3fb4997d835d135b01707adada153ecd

--- packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -22,6 +22,7 @@ import com.android.systemui.qs.shared.model.TileCategory
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import com.android.systemui.qs.tiles.AmbientDisplayTile
 import com.android.systemui.qs.tiles.AODTile
+import com.android.systemui.qs.tiles.AutoBrightnessTile
 import com.android.systemui.qs.tiles.CaffeineTile
 import com.android.systemui.qs.tiles.HeadsUpTile
 import com.android.systemui.qs.tiles.ReadingModeTile
@@ -51,6 +52,12 @@ interface CustomModule {
     @IntoMap
     @StringKey(AODTile.TILE_SPEC)
     fun bindAODTile(aodTile: AODTile): QSTileImpl<*>
+
+    /** Inject AutoBrightnessTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(AutoBrightnessTile.TILE_SPEC)
+    fun bindAutoBrightnessTile(autobrightnessTile: AutoBrightnessTile): QSTileImpl<*>
 
     /** Inject CaffeineTile into tileMap in QSModule */
     @Binds
@@ -91,6 +98,7 @@ interface CustomModule {
     companion object {
         const val AMBIENT_DISPLAY_TILE_SPEC = "ambient_display"
         const val AOD_TILE_SPEC = "aod"
+        const val AUTO_BRIGHTNESS_TILE_SPEC = "auto_brightness"
         const val CAFFEINE_TILE_SPEC = "caffeine"
         const val HEADS_UP_TILE_SPEC = "heads_up"
         const val READING_MODE_TILE_SPEC = "reading_mode"
@@ -123,6 +131,21 @@ interface CustomModule {
                     QSTileUIConfig.Resource(
                         iconRes = R.drawable.ic_qs_aod,
                         labelRes = R.string.quick_settings_aod_label
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.DISPLAY,
+            )
+
+        @Provides
+        @IntoMap
+        @StringKey(AUTO_BRIGHTNESS_TILE_SPEC)
+        fun provideAutoBrightnessTileConfig(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(AUTO_BRIGHTNESS_TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = R.drawable.ic_qs_autobrightness,
+                        labelRes = R.string.quick_settings_autobrightness_label
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.DISPLAY,
