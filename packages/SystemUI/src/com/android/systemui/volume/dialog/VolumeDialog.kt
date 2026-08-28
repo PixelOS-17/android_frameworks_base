VoltageOS-staging/frameworks_base_new

SystemUI: VolumeDialog: Fix layout issues with left gravity
prevent clipping of landscape ringer dialog
Signed-off-by: Dmitrii <bankersenator@gmail.com>

packages/SystemUI/src/com/android/systemui/volume/dialog/VolumeDialog.kt
@@ -73,19 +73,22 @@ constructor(
 
     private fun applyLayoutAndGravity() {
         val win = window ?: return
+        val dialogView = win.decorView
+
+        dialogView.layoutDirection = if (volumePanelOnLeft) View.LAYOUT_DIRECTION_RTL else View.LAYOUT_DIRECTION_LTR
 
         if (isVolumeDialogVertical) {
             win.setLayout(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.MATCH_PARENT,
             )
-            win.setGravity(if (volumePanelOnLeft) Gravity.START else Gravity.END)
+            win.setGravity(if (volumePanelOnLeft) Gravity.LEFT else Gravity.RIGHT)
         } else {
             win.setLayout(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT,
             )
-            val side = if (volumePanelOnLeft) Gravity.START else Gravity.END
+            val side = if (volumePanelOnLeft) Gravity.LEFT else Gravity.RIGHT
             win.setGravity(Gravity.TOP or side)
         }
     }

PixelOS-17/android_frameworks_base

Change-Id: Icc992d2af60c14fe4dff749bc2a28997eebf0d4a
(cherry picked from commit c384e77ecc97c650a72445df56581d54f113be2d)

packages/SystemUI/src/com/android/systemui/volume/dialog/VolumeDialog.kt
@@ -92,21 +92,24 @@ constructor(
 
     private fun applyLayoutAndGravity() {
         val win = window ?: return
-
+        val dialogView = win.decorView
         val isLeft = isLandscape() && volumePanelOnLeftLand ||
             !isLandscape() && volumePanelOnLeft
+
+        dialogView.layoutDirection = if (isLeft) View.LAYOUT_DIRECTION_RTL else View.LAYOUT_DIRECTION_LTR
+
         if (isVolumeDialogVertical) {
             win.setLayout(
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.MATCH_PARENT,
             )
-            win.setGravity(if (isLeft) Gravity.START else Gravity.END)
+            win.setGravity(if (isLeft) Gravity.LEFT else Gravity.RIGHT)
         } else {
             win.setLayout(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT,
             )
-            val side = if (isLeft) Gravity.START else Gravity.END
+            val side = if (isLeft) Gravity.LEFT else Gravity.RIGHT
             win.setGravity(Gravity.TOP or side)
         }
     }